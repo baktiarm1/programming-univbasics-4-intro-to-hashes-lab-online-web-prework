6e47460e559c1c289e9af539d5883a33d987cd9a
Baktiar Miah <baktiar.m@gmail.com>
2019-08-02 17:42:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,7 +9,7 @@ end
 
 def pioneer(key, value)
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
- hash = {key: 'value}
+ hash = {key: 'value'}
 
  return hash
   pioneer(name, 'Grace Hopper')
